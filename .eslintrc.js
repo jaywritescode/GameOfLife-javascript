4665a0b2ae6e142aa26fc17a1e06786cfabbc66c
Jay Harris <jayharris@gmail.com>
2016-12-26 10:57:49 -0500

    modify linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,31 @@
 module.exports = {
     "env": {
         "browser": true,
+        "node": true,
         "es6": true
     },
     "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
+    "parser": "babel-eslint",
     "plugins": [
         "react"
     ],
     "rules": {
         "indent": [
             "error",
-            4
+            2,
+            {"VariableDeclarator": {"var": 2, "let": 2, "const": 3}}
         ],
         "linebreak-style": [
             "error",
             "unix"
         ],
         "quotes": [
-            "error",
-            "single"
+            "warn",
+            "single",
+            {"avoidEscape": true}
         ],
         "semi": [
-            "error",
+            "warn",
             "always"
         ]
     }
