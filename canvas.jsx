af5ddf930c4a0951072fe015e16fee8300d1fa3f
Jay Harris <jayharris@gmail.com>
2016-12-27 13:49:28 -0500

    add Canvas#draw method

--- canvas.jsx
+++ canvas.jsx
@@ -5,8 +5,38 @@ export default class Canvas extends React.Component {
     return false;
   }
 
-  draw(state) {
+  /**
+   * @typedef {Object} CanvasState
+   * @property {Integer} xleft The left-most x-coordinate of the canvas
+   * @property {Integer} ytop The top-most y-coordinate of the canvas
+   * @property {Array} grid A two-dimensional array, grid[i][j] is truthy iff
+   *  the cell at (i,j) is live
+   * @property {Integer} magnify The current magnification level (x 100%)
+   */
 
+  /**
+   * Draws the grid given the game state.
+   *
+   * @param {CanvasState} the state of the game
+   */
+  draw(state) {
+    var context, m = state.magnify || 1;
+    var canvas = document.getElementById('canvas');
+    var rows = state.grid.length, columns = state.grid[0].length;
+
+    if (!canvas || !canvas.getContext) {
+      return;
+    }
+
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for(var y = canvas.height / 2 + (ytop * m), row = 0; row < rows; y += m, ++row) {
+      for(var x = canvas.width / 2 + (xleft * m), col = 0; col < columns; x += m, ++col) {
+        if(grid[row][col]) {
+          context.fillRect(x, y, m, m);
+        }
+      }
+    }
   }
 
   render() {
